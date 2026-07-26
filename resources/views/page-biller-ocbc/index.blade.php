64e94f3fbb51571cf02f9a8915246475e3a5bfaa
Fadly safyuddin maulana <fadly.m73@yahoo.com>
2026-07-27 00:12:29 +0700

    feat: update OCBC biller microsite
    - update biller OCBC styling
    - update biller OCBC page layout and content

--- resources/views/page-biller-ocbc/index.blade.php
+++ resources/views/page-biller-ocbc/index.blade.php
@@ -20,7 +20,7 @@
             </a>
         </div>
 
-        <div class="hero-image-panel">
+        <div class="hero-image-panel hero-bg-image">
             <img src="https://images.unsplash.com/photo-1586637383694-3ab1a6c70714?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="OCBC mobile app">
         </div>
