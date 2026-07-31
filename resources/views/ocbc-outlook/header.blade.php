06a9351b2e4f4f052a7cd56d845faa96b79dce93
Fadly safyuddin maulana <fadly.m73@yahoo.com>
2026-07-31 08:28:33 +0700

    feat: update OCBC Outlook header and landing page

--- resources/views/ocbc-outlook/header.blade.php
+++ resources/views/ocbc-outlook/header.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-gray-900">
+<header class="bg-transparent text-black">
     <nav aria-label="Global" class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
